08f95c3ef1171c4e27aef85f73d17ae0001a8a0d
imranolas <imranolas@gmail.com>
2016-03-01 22:53:47 +0000

    Attain enlightenment

--- src/koans/14_recursion.clj
+++ src/koans/14_recursion.clj
@@ -3,21 +3,32 @@
 
 (defn is-even? [n]
   (if (= n 0)
-    __
-    (___ (is-even? (dec n)))))
+    true
+    (not (is-even? (dec n)))))
 
+; Loops through all the number to 0 toggling the boolean acc
+; on each iteration. The value of acc upon reaching 0 represents
+; whether or the neumber is even or not.
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-      __
-      (recur (dec n) (not acc)))))
+        acc
+        (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll
+        reversed '()]
+    (if (empty? coll)
+        reversed
+        (recur (rest coll) (cons (first coll) reversed)))))
 
 (defn factorial [n]
-  __)
+  (loop [n n
+         acc 1]
+    (if (= n 0)
+        acc
+        (recur (dec n) (* acc n)))))
 
 (meditations
   "Recursion ends with a base case"
--- src/koans/15_destructuring.clj
+++ src/koans/15_destructuring.clj
@@ -1,4 +1,4 @@
-(ns koans.15-destructuring
+(ns koans.14-destructuring
   (:require [koan-engine.core :refer :all]))
 
 (def test-address
@@ -8,37 +8,46 @@
 
 (meditations
   "Destructuring is an arbiter: it breaks up arguments"
-  (= __ ((fn [[a b]] (str b a))
+  (= ":bar:foo" ((fn [[a b]] (str b a))
          [:foo :bar]))
 
   "Whether in function definitions"
-  (= (str "An Oxford comma list of apples, "
-          "oranges, "
-          "and pears.")
-     ((fn [[a b c]] __)
-      ["apples" "oranges" "pears"]))
+  (= (str "First comes love, "
+          "then comes marriage, "
+          "then comes Clojure with the baby carriage")
+     ((fn [[a b c]]
+        (format "First comes %s, then comes %s, then comes %s with the baby carriage"
+                a b c))
+      ["love" "marriage" "Clojure"]))
 
   "Or in let expressions"
-  (= "Rich Hickey aka The Clojurer aka Go Time aka Lambda Guru"
+  (= "Rich Hickey aka The Clojurer aka Go Time aka Macro Killah"
      (let [[first-name last-name & aliases]
-           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Lambda Guru")]
-       __))
+           (list "Rich" "Hickey" "The Clojurer" "Go Time" "Macro Killah")]
+       (str (format "%s %s aka " first-name last-name)
+            (clojure.string/join " aka " aliases))))
 
   "You can regain the full argument if you like arguing"
   (= {:original-parts ["Stephen" "Hawking"] :named-parts {:first "Stephen" :last "Hawking"}}
      (let [[first-name last-name :as full-name] ["Stephen" "Hawking"]]
-       __))
+       {:original-parts full-name
+        :named-parts {:first first-name
+                      :last last-name}}))
 
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-       __))
+       (clojure.string/join ", " [street-address city state])))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-       __))
+     (let [{:keys [street-address city state]} test-address]
+       (clojure.string/join ", " [street-address city state])))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address)))
+     ((fn [[first-name last-name]
+           {:keys [street-address city state]}]
+        (str (format "%s %s, " first-name last-name)
+             (clojure.string/join ", " [street-address city state])))
+      ["Test" "Testerson"] test-address)))
